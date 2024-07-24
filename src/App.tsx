eb966ba75f15cf56ebe6136d4bed7a7e5dbb606f
Murage-Ivy <ivymurage2000@gmail.com>
2024-07-24 12:52:02 +0200

    feat: roll die only if it isHeld is false

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,10 @@ type DiceProps = {
   value: number,
   isHeld: boolean
 }
+
 function App() {
   const [dice, setDice] = useState<DiceProps[]>(allNewDice())
+
   function allNewDice(): DiceProps[] {
     const diceArray: DiceProps[] = Array(10).fill(true).map(() => (
       {
@@ -18,19 +20,30 @@ function App() {
       }))
     return diceArray
   }
+
+  function handleRollDice(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault()
+    setDice(prevDice => {
+      return prevDice.map(die => {
+        return die.isHeld ? die : {
+          id: nanoid(),
+          value: Math.floor(Math.random() * 6) + 1,
+          isHeld: false
+        }
+      })
+    })
+  }
+
   function holdDice(diceId: string) {
     console.log(diceId)
-    setDice(oldDice => oldDice.map(die => die.id === diceId ? ({ ...die, isHeld: true }) : die))
+    setDice(oldDice => oldDice.map(die => die.id === diceId ? ({ ...die, isHeld: !die.isHeld }) : die))
   }
   const diceList = dice.map((die) => <Die
     holdDice={() => holdDice(die.id)}
     key={die.id} styles={{ backgroundColor: die.isHeld ? '#59E391' : '#F5F5F5' }}
     value={die.value} />)
 
-  function handleRollDice(e: React.MouseEvent<HTMLButtonElement>) {
-    e.preventDefault()
-    setDice(allNewDice())
-  }
+
   return (
     <main className="bg-primary flex items-center justify-center max-w-md m-auto py-4 px-3 mt-5 text-xs rounded-md ">
       <div className="bg-secondary m-5 gap-5 rounded-md p-5 grid grid-cols-5 grid-rows-2 items-center max-w-sm">
