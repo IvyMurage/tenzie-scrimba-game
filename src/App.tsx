d4e6aca3f559482f782fd026a9b57cde88c3811e
Murage-Ivy <ivymurage2000@gmail.com>
2024-07-24 09:29:48 +0200

    feat: Create an app main element and style it

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <main className="bg-primary max-w-md m-auto py-4 px-3 mt-5 text-xs rounded-md ">
+      <div className="bg-secondary m-5 rounded-md p-3 max-w-sm">
+        tenzie game
+
+      </div>
+    </main>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#0B2434',
+        secondary:'#F5F5F5',
+        tertiary: '##59E391'
+      },
+      fontFamily:{
+        inter: ['Inter', 'san-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
