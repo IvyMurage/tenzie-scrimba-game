f2f6d8a6f2fdbfd4085be18e0b6b280043da75d9
Murage-Ivy <ivymurage2000@gmail.com>
2024-07-24 10:43:56 +0200

    feat: display a die list

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import { useState } from "react"
 import Die from "./Die"
 
 function App() {
+  const [dice] = useState(allNewDice())
+  function allNewDice(): number[] {
+    const diceArray: number[] = Array(10).fill(true).map(() => Math.floor(Math.random() * 6) + 1)
+    return diceArray
+  }
+  const diceList = dice.map((die, index) => <Die key={`#dice-${index}`} value={die} />)
   return (
     <main className="bg-primary flex items-center justify-center max-w-md m-auto py-4 px-3 mt-5 text-xs rounded-md ">
       <div className="bg-secondary m-5 gap-5 rounded-md p-5 grid grid-cols-5 grid-rows-2 items-center max-w-sm">
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
+        {diceList}
       </div>
     </main>
   )
--- src/Die.tsx
+++ src/Die.tsx
@@ -1,6 +1,6 @@
 function Die(props: { value: number }) {
     return (
-        <div className='p-2 shadow-md rounded-md w-9 flex items-center justify-center text-md font-bold bg-white'>{props.value}</div>
+        <div className='p-2 shadow-md rounded-md w-9 cursor-pointer flex items-center justify-center text-md font-bold bg-white'><h2>{props.value}</h2></div>
     )
 }
 
