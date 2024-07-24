ce48e539951662e9fe75a5041d19234b72e9f329
Murage-Ivy <ivymurage2000@gmail.com>
2024-07-24 14:30:25 +0200

    feat: add useEffect hook to check if all values and isHeld property in the array are the same

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Die from "./Die"
 import { nanoid } from "nanoid"
 
@@ -10,14 +10,23 @@ type DiceProps = {
 
 function App() {
   const [dice, setDice] = useState<DiceProps[]>(allNewDice())
+  const [tenzies, setTenzie] = useState(false)
 
+
+  useEffect(() => {
+    dice.every(die => die.isHeld === true) && new Set(dice.map(die => die.value)).size && setTenzie(true)
+  }, [dice])
+  console.log(tenzies)
+  function generateNewDice() {
+    return ({
+      id: nanoid(),
+      value: Math.floor(Math.random() * 6) + 1,
+      isHeld: false
+    })
+  }
   function allNewDice(): DiceProps[] {
     const diceArray: DiceProps[] = Array(10).fill(true).map(() => (
-      {
-        id: nanoid(),
-        value: Math.floor(Math.random() * 6) + 1,
-        isHeld: false
-      }))
+      generateNewDice()))
     return diceArray
   }
 
@@ -25,11 +34,7 @@ function App() {
     e.preventDefault()
     setDice(prevDice => {
       return prevDice.map(die => {
-        return die.isHeld ? die : {
-          id: nanoid(),
-          value: Math.floor(Math.random() * 6) + 1,
-          isHeld: false
-        }
+        return die.isHeld ? die : generateNewDice()
       })
     })
   }
@@ -47,6 +52,11 @@ function App() {
   return (
     <main className="bg-primary flex items-center justify-center max-w-md m-auto py-4 px-3 mt-5 text-xs rounded-md ">
       <div className="bg-secondary m-5 gap-5 rounded-md p-5 grid grid-cols-5 grid-rows-2 items-center max-w-sm">
+        <div className=" col-span-full place-self-center text-center">
+          <h1 className="font-bold text-lg">Tenzies</h1>
+          <p className="text-base mt-4">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        </div>
+
         {diceList}
         <button onClick={handleRollDice} className=" border-0 font-inter bg-primary rounded-md font-semibold w-fit place-self-center text-secondary px-5 py-2 col-span-full">
           Roll
